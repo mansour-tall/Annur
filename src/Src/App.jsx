844c9eebaa0e6a74db6950994b1dce46e1f24f24
mansour-tall <mansour_tall@outlook.fr>
2026-03-16 21:23:19 +0400

    Create App.jsx

---
+++ src/Src/App.jsx
@@ -0,0 +1,8 @@
+export default function Annur() {
+  return (
+    <div style={{ padding: 40, fontFamily: "Arial" }}>
+      <h1>Annu'R</h1>
+      <p>Le premier annuaire digital de La Réunion.</p>
+    </div>
+  );
+}
